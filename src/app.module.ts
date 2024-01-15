8fc9fe2f20e7ca8965bf3a8f8ab8581056094993
ThibautRingeval <thibaut.ringeval@epsi.fr>
2024-01-15 10:54:12 +0100

    Demarrage de l'apprentissage de nestJS

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal:true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
